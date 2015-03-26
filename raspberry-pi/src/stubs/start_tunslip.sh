cee0c734cb2eae4426dd17172427ceb06d0ebb77
spiegelm <spiagl.m@gmail.com>
2015-03-26 14:29:31 +0100

    Deleted duplicates from src, moved start_tunslip.sh to src

--- raspberry-pi/src/stubs/start_tunslip.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-sleep 2
-sudo echo 'tunslip6 -s /dev/ttyUSB0 -c 25 fdfd::1/64 > /var/log/tunslip6' | at now
-#echo 'tunslip6 -s /dev/ttyUSB0 -c 25 fdfd::1/64 >> /var/log/tunslip6' | at now
