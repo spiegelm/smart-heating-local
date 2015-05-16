01014d01592a2428b9b3102eb6e3a3e486a35b47
spiegelm <spiagl.m@gmail.com>
2015-05-16 17:37:19 +0200

    Traverse server REST API. Upload measurements. Rename to server.py

--- raspberry-pi/src/server.py
+++ raspberry-pi/src/server.py
@@ -1,23 +1,66 @@
 #!/usr/bin/env python3
+import json
 
 import requests
 import subprocess
 
-server_url = 'http://52.28.68.182:8000/'
 
+class Server:
+    SERVER_URL = 'http://52.28.68.182:8000/'
 
-def main():
+    def thermostat_url(self, rfid=None):
+        url = self.SERVER_URL + 'thermostat/'
+        if rfid is not None:
+            url += rfid + '/'
+        return url
 
-    thermostats = LinkedThermostats().get_thermostats_macs()
-    print(thermostats)
+    def device_thermostat_url(self, rfid=None):
+        url = self.SERVER_URL + 'device/thermostat/'
+        if rfid is not None:
+            url += rfid + '/'
+        return url
 
-class LinkedThermostats:
+    def device_thermostat_lookup_url(self, mac):
+        return self.SERVER_URL + 'device/thermostat/lookup/?mac=%s' % mac
+
+    def device_thermostat(self, rfid=None, mac=None):
+        if (rfid is None) == (mac is None):
+            raise Exception('Either rfid or mac is required')
+
+        # Determine url
+        if mac is not None:
+            url = self.device_thermostat_lookup_url(mac)
+        else:
+            assert rfid is not None
+            url = self.device_thermostat_url(rfid)
+
+        r = requests.get(url)
+        assert(200 <= r.status_code < 300)
+
+        return r.json()
+
+    def temperature_url(self, thermostat_url):
+        return thermostat_url + 'temperature/'
+
+    def upload_measurement(self, temperature_measurement):
+        mac = temperature_measurement.mac
+        device_thermostat = self.device_thermostat(mac=mac)
+
+        thermostat_url = device_thermostat.get('thermostat').get('url')
+        data = json.dumps({'datetime': temperature_measurement.date.isoformat(),
+                'value': temperature_measurement.temperature})
+        headers = {'Content-type': 'application/json', 'Accept': 'application/json'}
+
+        r = requests.post(self.temperature_url(thermostat_url), data=data, headers=headers)
+
+        if not (200 <= r.status_code < 300):
+            raise Exception('Failed to upload measurement %s, Result: %s' % (temperature_measurement, r))
 
     def get_thermostats_macs(self):
 
         macs = []
         for rfid in self.get_thermostats_rfids():
-            r = requests.get(server_url + 'device/thermostat/' + rfid)
+            r = requests.get(self.SERVER_URL + 'device/thermostat/' + rfid)
             data = r.json()
             macs.append(data.get('mac'))
 
@@ -26,7 +69,7 @@ class LinkedThermostats:
     def get_thermostats_rfids(self):
 
         raspberry_mac = self.get_local_mac_address()
-        lookup_url = server_url + 'device/raspberry/lookup/?mac=' + raspberry_mac
+        lookup_url = self.SERVER_URL + 'device/raspberry/lookup/?mac=' + raspberry_mac
         r = requests.get(lookup_url)
 
         if r.status_code == 404:
@@ -81,8 +124,3 @@ class LinkedThermostats:
         mac = lines[hwaddr_index + 1]
 
         return mac
-
-
-
-if __name__ == "__main__":
-    main()
