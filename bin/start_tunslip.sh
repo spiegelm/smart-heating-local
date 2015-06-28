43f628992974d5877607247275afb90954f9ae04
spiegelm <spiagl.m@gmail.com>
2015-06-28 05:18:30 +0200

    Avoid tunslip6 call if interface is already up

--- bin/start_tunslip.sh
+++ bin/start_tunslip.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 sleep 2
-sudo echo 'tunslip6 -s /dev/ttyUSB0 -c 25 fdfd::1/64 > /var/log/tunslip6' | at now
-#echo 'tunslip6 -s /dev/ttyUSB0 -c 25 fdfd::1/64 >> /var/log/tunslip6' | at now
+# Check if tunnel interface is already up or establish one
+ifconfig tun0 || sudo echo 'tunslip6 -s /dev/ttyUSB0 -c 25 fdfd::1/64 > /var/log/tunslip6' | at now
